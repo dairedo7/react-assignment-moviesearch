d97cecbce4c55299f9c696676d337db2a3d2ed5a
Vladyslav Svitana <vladik719@gmail.com>
2022-08-06 19:05:00 +0200

    Update MoviesPageView.js

--- src/views/MoviesPageView/MoviesPageView.js
+++ src/views/MoviesPageView/MoviesPageView.js
@@ -40,7 +40,7 @@ export default function MoviesView() {
       pathname: navigate.location,
       search: `?query=${query}`,
     });
-    // navigate(`?query=${query}`, { search: `?query=${query}` });
+    // navigate(`?query=${query}`, { search: `?query=${query}` })
   };
 
   return (
